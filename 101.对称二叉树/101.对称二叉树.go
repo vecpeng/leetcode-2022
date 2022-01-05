df3955a96fbedfd8302754ce2ff406ec3bc8851b
vecpeng <841298391@qq.com>
2022-01-05 18:07:00 +0800

    :sparkles:feat(tree): add solution to [101]对称二叉树

---
+++ 101.对称二叉树/101.对称二叉树.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=golang
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return falseisSymmetricHelper(root.Left, root.Right)
+}
+func isSymmetricHelper(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right != nil {
+		return false
+	}
+	if right == nil && left != nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return isSymmetricHelper(left.Left, right.Right) && isSymmetricHelper(left.Right, right.Left)
+}
+// @lc code=end
+
